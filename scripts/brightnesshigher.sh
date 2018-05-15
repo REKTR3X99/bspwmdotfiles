b925f4147d857cd18d54870a258b1eba58acdb4c
REKTR3X99 <ajaypnair99@gmail.com>
2018-05-16 01:25:57 +0530

    Fixed polybar Ethernet display, added scripts to increase and decrease brightness of the display ( uses xrandr and user has to change the output display), changed the volume raise,lower and mute keys to keyboard bindings, mouse bindings will also work.

---
+++ scripts/brightnesshigher.sh
@@ -0,0 +1,10 @@
+brightness=$(xrandr --verbose | grep -i brightness | sed "s/[^0-9^.]//g")
+factor=0.1
+upperlimit=1.0
+newbrightness=$(echo "$brightness+$factor" | bc)
+
+if [ $(echo "$newbrightness <= $upperlimit" | bc) -eq 1 ]
+then
+xrandr --output DVI-D-0 --brightness $newbrightness
+
+fi
